8cff9f7a94241a461decf6c2d6ff207b5cacbc75
geethadineshs <geethamcasweet@gmail.com>
2024-07-29 15:36:22 +0530

    created  file

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+// App 1: Serves at port 3000
+const app1 = express();
+const port1 = 3000;
+
+app1.get('/', (req, res) => {
+    res.send('Hello, this is geetha.com!');
+});
+
+app1.listen(port1, () => {
+    console.log(`App1 listening at http://localhost:${port1}`);
+});
+
+// App 2: Serves at port 3001
+const app2 = express();
+const port2 = 3001;
+
+app2.get('/node', (req, res) => {
+    res.send('Hello from /node route!');
+});
+
+app2.listen(port2, () => {
+    console.log(`App2 listening at http://localhost:${port2}`);
+});
