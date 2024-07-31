753dcc88cf4b27cf1719076daa4d8e97640f9cfc
geethadineshs <162040133+geethadineshs@users.noreply.github.com>
2024-07-31 12:39:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,14 +12,3 @@ app1.listen(port1, () => {
     console.log(`App1 listening at http://localhost:${port1}`);
 });
 
-// App 2: Serves at port 3001
-const app2 = express();
-const port2 = 3001;
-
-app2.get('/node', (req, res) => {
-    res.send('Hello from /node route!');
-});
-
-app2.listen(port2, () => {
-    console.log(`App2 listening at http://localhost:${port2}`);
-});
